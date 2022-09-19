648a5468da935f830c3c13d0e0a93c24c11fecc1
Mateodans <90775644+Mateodans@users.noreply.github.com>
2022-09-19 00:05:29 -0300

    base de datos con problema

--- app/Models/recetas.php
+++ app/Models/recetas.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class recetas extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function pasos()
+    {
+        return $this->hasMany(Pasos::class);
+    }
+
+    public function ingredientes()
+    {
+        return $this->hasMany(Ingredientes::class);
+    }
 }
